f5a8c52dd7b93ad4b9a591a54a50bead3b639818
Sage Silberman <ssilberman2023@chadwickschool.org>
2021-09-30 09:41:55 -0700

    style: Change comment format

--- DoubleLetters/src/DoubleLetters.java
+++ DoubleLetters/src/DoubleLetters.java
@@ -1,15 +1,21 @@
-import java.util.Scanner; //import scanner!!
+
+//import scanner!!
+import java.util.Scanner;
 
 public class DoubleLetters {
 	public static void main(String[] args) {
-		Scanner scanText = new Scanner(System.in); // Create a Scanner object
+		// Create a Scanner object
+		Scanner scanText = new Scanner(System.in);
 
 		System.out.println("Enter a text");
-		String initialText = scanText.nextLine(); // save the initial text
+		// save the initial text
+		String initialText = scanText.nextLine();
 		String finalText = "";
 
-		for (int i = 0; i < initialText.length(); i++) { // for every thing inside the initial text it will double
-			char c = initialText.charAt(i); // creating a simple data thing that will add
+		// for every thing inside the initial text it will double
+		for (int i = 0; i < initialText.length(); i++) {
+			// creating a simple data thing that will add
+			char c = initialText.charAt(i);
 			if (Character.isLetter(c)) {
 				finalText = finalText + initialText.substring(i, i + 1) + initialText.substring(i, i + 1);
 			}
@@ -18,8 +24,9 @@ public class DoubleLetters {
 				finalText = finalText + initialText.substring(i, i + 1);
 			}
 
-			if (initialText.substring(i, i + 1).equals("!")) { // if the initial text is the final "term" (in terms of
-																// number of terms) and it equals ! then...
+			// if the initial text is the final "term" (in terms of
+			// number of terms) and it equals ! then...
+			if (initialText.substring(i, i + 1).equals("!")) {
 				finalText = finalText + "!!";
 
 			}
